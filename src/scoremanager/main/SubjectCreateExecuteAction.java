75745f19ca30bbb9b4f6482cd464e8cd750509f0
oohara <oohara@DESKTOP-0HA753D>
2024-05-01 11:31:30 +0900

    2024/05/01

--- src/scoremanager/main/SubjectCreateExecuteAction.java
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import bean.School;
 import bean.Subject;
 import bean.Teacher;
+import dao.ClassNumDao;
 import dao.SubjectDao;
 import tool.Action;
 
@@ -17,49 +18,52 @@ public class SubjectCreateExecuteAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		//ローカル変数の宣言 1
+
 		School school = new School();
-		school.setCd("oom");//学校コードをセットする
-		school.setName("テスト校");//学校名をセットする
+		school.setCd("oom");
+		school.setName("テスト校");
 
 		Teacher teacher = new Teacher();
-		teacher.setId("admin1");//講師IDをセット
-		teacher.setName("管理者１");//講師名をセット
-		teacher.setPassword("password");//ログインパスワードをセット
-		teacher.setSchool(school);//学校オブジェクトをセット
+		teacher.setId("admin1");
+		teacher.setName("管理者１");
+		teacher.setPassword("password");
+		teacher.setSchool(school);
 
-
-
-		//ローカル変数の宣言 1
 		//HttpSession session = req.getSession();//セッション
-		List<Subject> subjects = null;// 学生リスト
 		SubjectDao sDao = new SubjectDao();//学生Dao
 		String cd = "";//科目コード
 		String name = "";//科目名
+		Subject subject = null;//科目
+		List<Subject> subjects = null;// 学生リスト
 		Map<String, String> errors = new HashMap<>();// エラーメッセージ
-		//ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
+		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
 		//Teacher teacher = (Teacher) session.getAttribute("user");// ログインユーザーを取得
 
 		//リクエストパラメータ―の取得 2
-		cd = req.getParameter("cd");//科目コード
-		name = req.getParameter("name");//科目名
+		cd = req.getParameter("cd");//学生番号
+		name = req.getParameter("name");//氏名
 
 		//DBからデータ取得 3
+		subject = sDao.get(cd);// 学生番号から学生インスタンスを取得
 		subjects = sDao.filter(teacher.getSchool());
-		//subjects = sDao.get(cd);// 科目コードから学生インスタンスを取得
 
 		//ビジネスロジック 4
 		//DBへデータ保存 5
-		//条件で手順4~5の内容が分岐else{
-			if (subjects == null) {// 学生が未登録だった場合
-				subjects = new Subject();
-				// インスタンスに値をセット
-				subjects.setCd(cd);
-				subjects.setName(name);
-				// 学生を保存
-				sDao.save(subjects);
-			} else {//入力された学番がDBに保存されていた場合
-				errors.put("cd", "科目コードが重複しています");
-			}
+		//条件で手順4~5の内容が分岐
+		if (subject == null) {// 学生が未登録だった場合
+			// 学生インスタンスを初期化
+			subject = new Subject();
+			// インスタンスに値をセット
+			subject.setCd(cd);
+			subject.setName(name);
+			subject.setSchool(school);
+			//student.setSchool(((Teacher)session.getAttribute("user")).getSchool());
+			// 学生を保存
+			sDao.save(subject);
+		} else {//入力された学番がDBに保存されていた場合
+			errors.put("cd", "学生番号が重複しています");
+		}
 
 		//エラーがあったかどうかで手順6~7の内容が分岐
 		//レスポンス値をセット 6
@@ -68,6 +72,9 @@ public class SubjectCreateExecuteAction extends Action {
 		if(!errors.isEmpty()){
 			// リクエスト属性をセット
 			req.setAttribute("errors", errors);
+			req.setAttribute("cd", cd);
+			req.setAttribute("name", name);
+			req.getRequestDispatcher("subject_create.jsp").forward(req, res);
 			return;
 		}
 		req.getRequestDispatcher("subject_create_done.jsp").forward(req, res);
