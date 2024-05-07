10880909c6bb5baa9693608c7b45cf8a0a0bab1d
shna0 <shuuuma.0605@gmail.com>
2024-05-07 10:22:12 +0900

    5/7

--- WebContent/scoremanager/main/subject_delete.jsp
+++ WebContent/scoremanager/main/subject_delete.jsp
@@ -8,13 +8,15 @@
 	</c:param>
 	<c:param name="scripts"></c:param>
 	<c:param name="content">
-	<h2 class="h3 mb-3 fw-norma bg-secondary bg-opacity-10 py-2 px-4">科目情報削除</h2>
-		<div class="col-4">
-			<label class="form-label" for="student-f1-select">「${name_set}(${cd_set})」を削除してもよろしいですか</label>
-			<div class="btn-seco">
-				<button class="btn btn-primary" name="end">削除</button><br>
-				<a href="SubjectList.action">戻る</a>
-			</div>
-		</div>
-	</c:param>
+		<section class="me-4">
+			<h2 class="h3 mb-3 fw-norma bg-secondary bg-opacity-10 py-2 px-4">科目情報削除</h2>
+				<div class="col-4">
+					<label class="form-label" for="student-f1-select">「${name_set}(${cd_set})」を削除してもよろしいですか</label>
+					<div class="btn-seco">
+						<button class="btn btn-primary" name="end">削除</button><br>
+						<a href="SubjectList.action">戻る</a>
+					</div>
+				</div>
+			</section>
+		</c:param>
 </c:import>
--- src/scoremanager/main/SubjectDeleteAction.java
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -1,11 +1,16 @@
 package scoremanager.main;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.School;
 import bean.Subject;
 import bean.Teacher;
+import dao.ClassNumDao;
 import dao.SubjectDao;
 import tool.Action;
 
@@ -23,16 +28,29 @@ public class SubjectDeleteAction extends Action {
 		teacher.setName("管理者１");
 		teacher.setPassword("password");
 		teacher.setSchool(school);
-		SubjectDao subDao = new SubjectDao();
+
+		//HttpSession session = req.getSession();//セッション
+		SubjectDao sDao = new SubjectDao();//学生Dao
 		String cd = "";//科目コード
 		String name = "";//科目名
-		Subject subject = null;
+		Subject subject = null;//科目
+		List<Subject> subjects = null;// 学生リスト
+		Map<String, String> errors = new HashMap<>();// エラーメッセージ
+		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
+		//Teacher teacher = (Teacher) session.getAttribute("user");// ログインユーザーを取得
 
-		cd = req.getParameter("cd");//科目コード
-		subject = subDao.get(cd);
+		//リクエストパラメータ―の取得 2
+		cd = req.getParameter("cd");//学生番号
+
+		//DBからデータ取得 3
+		subject = sDao.get(cd);// 学生番号から学生インスタンスを取得
+		subjects = sDao.filter(teacher.getSchool());
+
 		name = subject.getName();
+
 		req.setAttribute("cd_set", cd);
 		req.setAttribute("name_set", name);
+
 		System.out.println("★ ★ ★ ★ ★ ★ ★ ★ ");
 
 		req.getRequestDispatcher("subject_delete.jsp").forward(req, res);
