bce4205d4b25997927e410882c429874eb4a2e06
oohara <oohara@DESKTOP-DLMLFJF>
2024-05-16 11:33:48 +0900

    5月15日プログラム作成

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -3,31 +3,36 @@ package bean;
 import java.io.Serializable;
 
 public class Test implements Serializable {
-	private String student_no;
-	private String subject_cd;
-	private String school_cd;
+	private Student student;
+	private String classNum;
+	private Subject subject;
+	private School school;
 	private Integer no;
 	private Integer point;
-	private String class_num;
-	private School school;
 
-	public String getStudent_no() {
-		return student_no;
+	public Student getStudent() {
+		return student;
 	}
-	public void setStudent_no(String student_no) {
-		this.student_no = student_no;
+	public void setStudent(Student student) {
+		this.student = student;
 	}
-	public String getSubject_cd() {
-		return subject_cd;
+	public String getClassNum() {
+		return classNum;
 	}
-	public void setSubject_cd(String subject_cd) {
-		this.subject_cd = subject_cd;
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
 	}
-	public String getSchool_cd() {
-		return school_cd;
+	public Subject getSubject() {
+		return subject;
 	}
-	public void setSchool_cd(String school_cd) {
-		this.school_cd = school_cd;
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+	public School getSchool() {
+		return school;
+	}
+	public void setSchool(School school) {
+		this.school = school;
 	}
 	public Integer getNo() {
 		return no;
@@ -40,17 +45,5 @@ public class Test implements Serializable {
 	}
 	public void setPoint(Integer point) {
 		this.point = point;
-	}
-	public String getClass_num() {
-		return class_num;
-	}
-	public void setClass_num(String class_num) {
-		this.class_num = class_num;
-	}
-	public School getSchool() {
-		return school;
-	}
-	public void setSchool(School school) {
-		this.school = school;
 	}
 }
--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -5,7 +5,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.School;
+import bean.Student;
+import bean.Subject;
 import bean.Test;
 
 
@@ -13,8 +18,10 @@ import bean.Test;
 
 public class TestDao extends Dao{
 
+	private String baseSql = "select * from test ";
 
-public Test noget(String student_no) throws Exception {
+
+	public Test get(Student student, Subject subject, School school, int no) throws Exception {
 		//学生インスタンスを初期化
 		Test test = new Test();
 		//データベースへのコネクションを確立
@@ -24,25 +31,30 @@ public Test noget(String student_no) throws Exception {
 
 		try {
 			//プリペアードステートメントにSQL文をセット
-			statement = connection.prepareStatement("select * from student where student_no=?");
+			statement = connection.prepareStatement("select * from test where student_no=?, subject_cd=?, school_cd=?, no=?");
 			//プリペアードステートメントに学生番号をバインド
-			statement.setString(1, student_no);
+			statement.setString(1, student.getNo());
+			statement.setString(2, subject.getCd());
+			statement.setString(3, school.getCd());
+			statement.setInt(4, no);
 			//プリペアードステートメントを実行
 			ResultSet rSet = statement.executeQuery();
 
 			//学校Daoを初期化
+			StudentDao studentDao = new StudentDao();
+			SubjectDao subjectDao = new SubjectDao();
 			SchoolDao schoolDao = new SchoolDao();
+
 
 			if (rSet.next()) {
 				//リザルトセットが存在する場合
 				//学生インスタンスに検索結果をセット
-				test.setStudent_no(rSet.getString("student_no"));
-				test.setSubject_cd(rSet.getString("subject_cd"));
-				test.setSchool_cd(rSet.getString("school_cd"));
+				test.setStudent(studentDao.get(rSet.getString("student_no")));
+				test.setClassNum(rSet.getString("class_num"));
+				test.setSubject(subjectDao.get(rSet.getString("subject_cd")));
+				test.setSchool(schoolDao.get(rSet.getString("school_cd")));
 				test.setNo(rSet.getInt("no"));
 				test.setPoint(rSet.getInt("point"));
-				//学校フィールドには学校コードで検索した学校インスタンスをセット
-				test.setSchool(schoolDao.get(rSet.getString("school_cd")));
 			} else {
 				//リザルトセットが存在しない場合
 				//学生インスタンスにnullをセット
@@ -72,9 +84,87 @@ public Test noget(String student_no) throws Exception {
 		return test;
 	}
 
-	public boolean save(Test test) throws Exception {
+	private List<Test> postFilter(ResultSet rSet, School school) throws Exception {
+		//リストを初期化
+		List<Test> list = new ArrayList<>();
+		try {
+			StudentDao studentDao = new StudentDao();
+			SubjectDao subjectDao = new SubjectDao();
+			//リザルトセットを全権操作
+			while (rSet.next()) {
+				//学生インスタンスを初期化
+				Test test = new Test();
+				//学生インスタンスに検索結果をセット
+				test.setStudent(studentDao.get(rSet.getString("student_no")));
+				test.setClassNum(rSet.getString("class_num"));
+				test.setSubject(subjectDao.get(rSet.getString("subject_cd")));
+				test.setSchool(school);
+				test.setNo(rSet.getInt("no"));
+				test.setPoint(rSet.getInt("point"));
+				//リストに追加
+				list.add(test);
+			}
+		} catch (SQLException | NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+	public List<Test> filter(int entYear, String classNum, Subject subject, int num, School school) throws Exception {
+		//リストを初期化
+		List<Test> list = new ArrayList<>();
 		//コネクションを確立
 		Connection connection = getConnection();
+		//プリペアードステートメント
+		PreparedStatement statement = null;
+		//リザルトセット
+		ResultSet rSet = null;
+		//SQL文の条件
+		String condition = "inner join student on test.student_no = student.no where ent_year=? and test.class_num=? and subject_cd=? and test.school_cd=?";
+		//SQL文のソート
+		String order = " order by test.student_no asc";
+
+		try {
+			//プリペアードステートメントにSQL文をセット
+			statement = connection.prepareStatement(baseSql + condition + order);
+			//プリペアードステートメントに学校コードをバインド
+			statement.setInt(1, entYear);
+			//プリペアードステートメントに入学年度をバインド
+			statement.setString(2, classNum);
+			//プリペアードステートメントにクラス番号をバインド
+			statement.setString(3, subject.getCd());
+			//プライベートステートメントを実行
+			statement.setString(4, school.getCd());
+			//プライベートステートメントを実行
+			rSet = statement.executeQuery();
+			//リストへの格納処理を実行
+			list = postFilter(rSet, school);
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			//プリペアードステートメントを閉じる
+			if(statement != null) {
+				try{
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			//コネクションを閉じる
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+
+		return list;
+	}
+
+	public boolean save(Test test, Connection connection) throws Exception {
+		//コネクションを確立
 		//プリペアードステートメント
 		PreparedStatement statement = null;
 		//実行件数
@@ -82,30 +172,27 @@ public Test noget(String student_no) throws Exception {
 
 		try {
 			//データベースから学年を取得
-			Test old = get(test.getStudent_no(),test.getSubject_cd(),test.getSchool_cd(),test.getNo());
+			Test old = get(test.getStudent(),test.getSubject(),test.getSchool(),test.getNo());
 			if (old == null) {
 				//学年が存在しなかった場合
 				//プリペアードステートメントにINSERT文をセット
 				statement = connection.prepareStatement(
-						"insert into student(no, name, ent_year, class_num, is_attend, school_cd) values(?, ?, ?, ?, ?, ?)");
+						"insert into test(student_no, subject_cd, school_cd, no, point, class_num) values(?, ?, ?, ?, ?, ?)");
 				//プリペアードステートメントに値をバインド
-				statement.setString(1, student.getNo());
-				statement.setString(2, student.getName());
-				statement.setInt(3, student.getEntYear());
-				statement.setString(4, student.getClassNum());
-				statement.setBoolean(5, student.isAttend());
-				statement.setString(6, student.getSchool().getCd());
+				statement.setString(1, test.getStudent().getNo());
+				statement.setString(2, test.getSubject().getCd());
+				statement.setString(3, test.getSchool().getCd());
+				statement.setInt(4, test.getNo());
+				statement.setInt(5, test.getPoint());
+				statement.setString(6, test.getClassNum());
 			} else {
 				//学生が存在した場合
 				//プリペアードステートメントにUPDATE文をセット
 				statement = connection
-						.prepareStatement("update student set name =?, ent_year=?, class_num=?, is_attend=? where no=?");
+						.prepareStatement("update test set point=?, class_num=? where student_no=?, subject_cd=?, school_cd=?, no=?");
 				//プリペアードステートメントに値をバインド
-				statement.setString(1, student.getName());
-				statement.setInt(2, student.getEntYear());
-				statement.setString(3, student.getClassNum());
-				statement.setBoolean(4, student.isAttend());
-				statement.setString(5, student.getNo());
+				statement.setInt(1, test.getPoint());
+				statement.setString(2, test.getClassNum());
 			}
 
 			//プリペアードステートメントを実行
@@ -143,7 +230,7 @@ public Test noget(String student_no) throws Exception {
 
 	}
 
-	public void delete(int no) throws Exception {
+	public boolean delete(List<Test> ) throws Exception {
 		//コネクションを確立
 		Connection connection = getConnection();
 		//プリペアードステートメント
@@ -170,6 +257,36 @@ public Test noget(String student_no) throws Exception {
 				if (connection != null) {
 					try {
 						connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+	}
+	public boolean delete(Test test, Connection connection){
+		PreparedStatement statement = null;
+		try{
+			statement = connection
+					.prepareStatement("delete from test where student_no=?, subject_cd=?, school_cd=?, no=?");
+			statement.setString(1, test.getStudent().getNo());
+			statement.setString(2, test.getSubject().getCd());
+			statement.setString(3, test.getSchool().getCd());
+			statement.setInt(4, test.getNo());
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			//プリペアードステートメントを閉じる
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			//コネクションを閉じる
+			if (connection != null) {
+				try {
+					connection.close();
 				} catch (SQLException sqle) {
 					throw sqle;
 				}
